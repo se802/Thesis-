97bbc5aad443a2edb4fd38f8f453dcd25329f21d
UbuntuCS <stelios_evagorou@hotmail.com>
2023-03-25 12:57:51 +0200

    remove main.c

---
+++ cmake-build-debug/CMakeFiles/FuseProject.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FuseProject.dir/main.c.o"
+  "CMakeFiles/FuseProject.dir/main.c.o.d"
+  "CMakeFiles/FuseProject.dir/main2.c.o"
+  "CMakeFiles/FuseProject.dir/main2.c.o.d"
+  "FuseProject"
+  "FuseProject.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/FuseProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/stelios/CLionProjects/FuseProject
+# Install script for directory: /home/csdeptucy/CLionProjects/Thesis-
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/stelios/CLionProjects/FuseProject/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/csdeptucy/CLionProjects/Thesis-/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
